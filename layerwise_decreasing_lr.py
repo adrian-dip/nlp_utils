63bf813bfef0174fbe00ff69ac81a1ec7870bdb5
adrian-dip <dip.adrian@gmail.com>
2023-02-21 16:38:09 -0300

    Update layerwise_decreasing_lr.py

--- layerwise_decreasing_lr.py
+++ layerwise_decreasing_lr.py
@@ -18,18 +18,19 @@ def layerwise_lr(model, lr):
   ξ = 0.95         # decay factor by which to decrease (lr * epsilon = 5% per layer)
 
   parameters = []
-  prev_group_name = layers[2].split('.')[2]      # Spare the first layer from decreasing
+  prev_group_name = layers[2].split('.')[2]       
+                                                 # Check your model's layer names to make sure this line of code works
+                                                 # Current behavior: "encoder.layer.11.output.LayerNorm.bias" -> "11"
 
-  # store params & learning rates
   for idx, name in enumerate(layers):
 
       # parameter group name
       cur_group_name = name.split('.')[2]
 
       # update learning rate
-      if cur_group_name != prev_group_name:      # Spare the first layer from decreasing
-          lr_fn *= ξ
-      prev_group_name = cur_group_name
+      if cur_group_name != prev_group_name:      # Result: lr = 0.000017, encoder.layer.11.output.LayerNorm.bias,
+          lr_fn *= ξ                             # lr = 0.000016, encoder.layer.10.output.LayerNorm.bias,       
+      prev_group_name = cur_group_name           # etc. 
 
       # append layer parameters
       parameters += [{'params': [p for n, p in model.named_parameters() if n == name and p.requires_grad],
